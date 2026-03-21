4510bdde3397e5bacb53dc14e447072a4504f522
Tyler Allen <tyler@tallen.me>
2026-03-21 18:01:22 -0400

    Add Debug as env variable

--- config.env.py
+++ config.env.py
@@ -1,7 +1,7 @@
 import os
 
 # Flask config
-DEBUG = False
+DEBUG = lower(os.environ.get("DEBUG", "false")) == "true"
 IP = os.environ.get("IP", "0.0.0.0")
 PORT = os.environ.get("PORT", "8080")
 SERVER_NAME = os.environ.get("SERVER_NAME", "localhost:8080")
